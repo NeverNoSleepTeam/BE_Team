a9158271a804944b4d0a03385ddbb3580dc4b4a4
jaemoon <woans5970@naver.com>
2024-02-20 17:25:08 +0900

    create project

---
+++ src/main/java/NS/pgmg/PgmgApplication.java
@@ -0,0 +1,13 @@
+package NS.pgmg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgmgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PgmgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/NS/pgmg/PgmgApplicationTests.java
@@ -0,0 +1,13 @@
+package NS.pgmg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PgmgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
