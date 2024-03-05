9ce52fe3f4b2523ee22e42b6e3abdeefed362427
jaemoon <woans5970@naver.com>
2024-03-05 13:00:54 +0900

    Create: Duplicate Exception

---
+++ src/main/java/NS/pgmg/exception/EmailDuplicateException.java
@@ -0,0 +1,24 @@
+package NS.pgmg.exception;
+
+public class EmailDuplicateException extends RuntimeException{
+
+    public EmailDuplicateException() {
+        super();
+    }
+
+    public EmailDuplicateException(String message) {
+        super(message);
+    }
+
+    public EmailDuplicateException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public EmailDuplicateException(Throwable cause) {
+        super(cause);
+    }
+
+    protected EmailDuplicateException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
---
+++ src/main/java/NS/pgmg/exception/NameDuplicateException.java
@@ -0,0 +1,23 @@
+package NS.pgmg.exception;
+
+public class NameDuplicateException extends RuntimeException {
+    public NameDuplicateException() {
+        super();
+    }
+
+    public NameDuplicateException(String message) {
+        super(message);
+    }
+
+    public NameDuplicateException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NameDuplicateException(Throwable cause) {
+        super(cause);
+    }
+
+    protected NameDuplicateException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
