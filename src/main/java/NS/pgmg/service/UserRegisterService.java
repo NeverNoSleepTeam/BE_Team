6c4cb8ed1b1271110320da678762713a28a4b6bb
jaemoon <woans5970@naver.com>
2024-03-07 22:24:53 +0900

    Modify: UserRegisterService

--- src/main/java/NS/pgmg/service/UserRegisterService.java
+++ src/main/java/NS/pgmg/service/UserRegisterService.java
@@ -9,17 +9,27 @@ import NS.pgmg.exception.NameDuplicateException;
 import NS.pgmg.exception.PasswordMismatchException;
 import NS.pgmg.repository.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
-@RequiredArgsConstructor
+import java.io.File;
+import java.io.IOException;
+
+@Slf4j
 @Service
+@RequiredArgsConstructor
 public class UserRegisterService {
 
     private final UserRepository userRepository;
+
+    @Value("${file.dir}")
+    private String fileDir;
 
     @Transactional
     public ResponseEntity<String> createBasicUser(BasicRegisterDto request) {
@@ -59,13 +69,14 @@ public class UserRegisterService {
     }
 
     @Transactional
-    public ResponseEntity<String> createProPhotoUser(ProPhotoRegisterDto request) {
+    public ResponseEntity<String> createProPhotoUser(ProPhotoRegisterDto request, MultipartFile file) {
         String email = request.getEmail();
         User findUser = userRepository.findByEmail(email);
         if (findUser == null){
             return new ResponseEntity<>("잘못된 접근입니다.", HttpStatus.BAD_REQUEST);
         }
         findUser.setProPhotoInfo(request);
+        emptyFileCheck(file, email);
         userRepository.save(findUser);
         return new ResponseEntity<>("기사 회원가입이 완료되었습니다.", HttpStatus.CREATED);
     }
@@ -89,6 +100,20 @@ public class UserRegisterService {
         User findUser = userRepository.findByEmail(name);
         if (findUser != null) {
             throw new NameDuplicateException("이미 존재하는 닉네임입니다.");
+        }
+    }
+
+    public void emptyFileCheck(MultipartFile file, String email) {
+
+        if (file != null) {
+            String fullPath = fileDir + email + ".pdf";
+            log.info("파일 저장 fullPath = {}", fullPath);
+
+            try {
+                file.transferTo(new File(fullPath));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 }
