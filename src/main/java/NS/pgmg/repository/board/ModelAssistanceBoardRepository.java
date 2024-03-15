47fe5ca916d2acfbd5da8d0e6258a4a6263f633e
jaemoon <woans5970@naver.com>
2024-03-16 00:31:21 +0900

    Create: ModelAssistanceBoardRepository

---
+++ src/main/java/NS/pgmg/repository/board/ModelAssistanceBoardRepository.java
@@ -0,0 +1,8 @@
+package NS.pgmg.repository.board;
+
+import NS.pgmg.domain.board.ModelAssistanceBoard;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ModelAssistanceBoardRepository extends JpaRepository<ModelAssistanceBoard, Long> {
+    public ModelAssistanceBoard findByBidAndBase_Email(Long bid, String email);
+}
