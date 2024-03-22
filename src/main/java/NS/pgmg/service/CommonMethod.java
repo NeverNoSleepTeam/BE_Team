3392aabd68ef26377a3b72fc7d300bed177287ce
jaemoon <woans5970@naver.com>
2024-03-22 15:11:40 +0900

    Modify: CommonMethod - Add emailCheck Function

--- src/main/java/NS/pgmg/service/CommonMethod.java
+++ src/main/java/NS/pgmg/service/CommonMethod.java
@@ -1,6 +1,7 @@
 package NS.pgmg.service;
 
 import NS.pgmg.config.JwtConfig;
+import NS.pgmg.exception.TokenNullException;
 
 import java.util.UUID;
 
@@ -9,14 +10,18 @@ public class CommonMethod {
     protected static String tokenCheck(String token) {
 
         if (token == null) {
-            return "token is null";
+            throw new RuntimeException("토큰이 없습니다.");
         }
 
         return JwtConfig.getEmail(token);
     }
 
-    protected static boolean emailCheck(String tokenEmail, String requestEmail) {
-        return tokenEmail.equals(requestEmail);
+    protected static void emailCheck(String tokenEmail, String requestEmail) {
+
+        if (!tokenEmail.equals(requestEmail)) {
+            throw new RuntimeException("토큰 정보와 요청하신 이메일의 정보가 일치하지 않습니다.");
+        }
+
     }
 
     protected static String createRandomUuid() {
