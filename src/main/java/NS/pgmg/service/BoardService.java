84fa1654582f4af3917849e9ad3e2f7855520de4
jaemoon <woans5970@naver.com>
2024-03-22 15:11:16 +0900

    Modify: BoardService - Change Response Type, Change Exception Process

--- src/main/java/NS/pgmg/service/BoardService.java
+++ src/main/java/NS/pgmg/service/BoardService.java
@@ -7,7 +7,6 @@ import NS.pgmg.dto.board.BoardRequestDto;
 import NS.pgmg.dto.board.ModelAssistanceCreateDto;
 import NS.pgmg.dto.board.ModelAssistanceFindResponseDto;
 import NS.pgmg.dto.board.ModelAssistanceUpdateDto;
-import NS.pgmg.exception.TokenNullException;
 import NS.pgmg.repository.board.ModelAssistanceBoardRepository;
 import NS.pgmg.repository.user.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -40,11 +39,12 @@ public class BoardService {
                                                         MultipartFile title, List<MultipartFile> details) {
 
         try {
-            String email = tokenCheck(token);
+            String requestEmail = tokenCheck(token);
+            emailCheck(requestEmail, request.getEmail());
             String titleValue = saveTitleFile(title);
             List<String> detailsValue = saveDetailFiles(details);
 
-            BaseBoard base = BaseBoard.setBaseBoard(request, email, titleValue, detailsValue);
+            BaseBoard base = BaseBoard.setBaseBoard(request, requestEmail, titleValue, detailsValue);
             ModelAssistanceBoard modelAssistanceBoard = ModelAssistanceBoard.builder()
                     .base(base)
                     .modelAssistanceCategory(request.getModelAssistanceCategory())
@@ -55,43 +55,45 @@ public class BoardService {
 
             return ResponseEntity.ok().body(Map.of("message", "게시물이 생성됐습니다."));
         } catch (RuntimeException e) {
-            return ResponseEntity.ok().body(Map.of("message", e.getMessage()));
+            return ResponseEntity.badRequest().body(Map.of("message", e.getMessage()));
         }
     }
 
     @Transactional
     public ResponseEntity<?> findMA(String token, BoardRequestDto request) {
-        String requestEmail = tokenCheck(token);
+        try {
+            String requestEmail = tokenCheck(token);
 
-        if (!requestEmail.equals(request.getEmail())) {
-            return ResponseEntity.badRequest().body("토큰 정보와 요청하신 이메일의 정보가 일치하지 않습니다.");
+            emailCheck(requestEmail, request.getEmail());
+
+            User findUser = userRepository.findByEmail(requestEmail);
+            ModelAssistanceBoard findBoard = modelAssistanceBoardRepository.
+                    findByBidAndBase_Email(request.getId(), requestEmail);
+
+            BaseBoard findBase = findBoard.getBase();
+
+            return ResponseEntity.ok().body(ModelAssistanceFindResponseDto.builder()
+                    .name(findUser.getName())
+                    .createdAt(findBase.getCreatedAt())
+                    .title(findBase.getTitle())
+                    .price(findBoard.getPrice())
+                    .modelAssistanceCategory(findBoard.getModelAssistanceCategory())
+                    .firstDate(findBase.getFirstDate())
+                    .lastDate(findBase.getLastDate())
+                    .place(findBoard.getPlace())
+                    .gender(findUser.getGender())
+                    .height(findUser.getHeight())
+                    .weight(findUser.getWeight())
+                    .top(findUser.getTop())
+                    .bottom(findUser.getBottom())
+                    .shoes(findUser.getShoes())
+                    .nationality(findUser.getNationality())
+                    .city(findUser.getCity())
+                    .build()
+            );
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(Map.of("message", e.getMessage()));
         }
-
-        User findUser = userRepository.findByEmail(requestEmail);
-        ModelAssistanceBoard findBoard = modelAssistanceBoardRepository.
-                findByBidAndBase_Email(request.getId(), requestEmail);
-
-        BaseBoard findBase = findBoard.getBase();
-
-        return ResponseEntity.ok().body(ModelAssistanceFindResponseDto.builder()
-                        .name(findUser.getName())
-                        .createdAt(findBase.getCreatedAt())
-                        .title(findBase.getTitle())
-                        .price(findBoard.getPrice())
-                        .modelAssistanceCategory(findBoard.getModelAssistanceCategory())
-                        .firstDate(findBase.getFirstDate())
-                        .lastDate(findBase.getLastDate())
-                        .place(findBoard.getPlace())
-                        .gender(findUser.getGender())
-                        .height(findUser.getHeight())
-                        .weight(findUser.getWeight())
-                        .top(findUser.getTop())
-                        .bottom(findUser.getBottom())
-                        .shoes(findUser.getShoes())
-                        .nationality(findUser.getNationality())
-                        .city(findUser.getCity())
-                .build()
-        );
     }
 
     @Transactional
@@ -102,13 +104,11 @@ public class BoardService {
     @Transactional
     public ResponseEntity<Map<String, String>> updateMA(String token, ModelAssistanceUpdateDto request,
                                                      MultipartFile title, List<MultipartFile> details) {
-        String requestEmail = tokenCheck(token);
-
-        if (!requestEmail.equals(request.getEmail())) {
-            return ResponseEntity.ok().body(Map.of("message", "토큰 정보와 요청하신 이메일의 정보가 일치하지 않습니다."));
-        }
-
         try {
+            String requestEmail = tokenCheck(token);
+
+            emailCheck(requestEmail, request.getEmail());
+
             ModelAssistanceBoard findBoard = modelAssistanceBoardRepository
                     .findByBidAndBase_Email(request.getId(), request.getEmail());
 
@@ -131,23 +131,27 @@ public class BoardService {
             findBoard.setUpdateBoard(updateBase, request.getModelAssistanceCategory(), request.getPlace(), request.getPrice());
 
             modelAssistanceBoardRepository.save(findBoard);
+
+            return ResponseEntity.ok().body(Map.of("message", "게시물이 수정됐습니다."));
         } catch (RuntimeException e) {
-            return ResponseEntity.ok().body(Map.of("message", e.getMessage()));
+            return ResponseEntity.badRequest().body(Map.of("message", e.getMessage()));
         }
 
-        return ResponseEntity.ok().body(Map.of("message", "게시물이 수정됐습니다."));
     }
 
     @Transactional
     public ResponseEntity<Map<String, String>> deleteMA(String token, BoardRequestDto request) {
-        String requestEmail = tokenCheck(token);
+        try {
+            String requestEmail = tokenCheck(token);
 
-        if (!requestEmail.equals(request.getEmail())) {
-            return ResponseEntity.ok().body(Map.of("message", "토큰 정보와 요청하신 이메일의 정보가 일치하지 않습니다."));
+            emailCheck(requestEmail, request.getEmail());
+
+            modelAssistanceBoardRepository.deleteByBid(request.getId());
+            return ResponseEntity.ok().body(Map.of("message", "게시물이 삭제됐습니다."));
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(Map.of("message", e.getMessage()));
         }
 
-        modelAssistanceBoardRepository.deleteByBid(request.getId());
-        return ResponseEntity.ok().body(Map.of("message", "게시물이 삭제됐습니다."));
     }
 
     private String saveTitleFile(MultipartFile file) {
