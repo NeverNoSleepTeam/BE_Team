10f847e8bc7b02eb66a2085173db10a84eb09596
jaemoon <woans5970@naver.com>
2024-03-25 22:42:37 +0900

    Modify: ModelAssistanceService - move function(CommonMethod)

--- src/main/java/NS/pgmg/service/board/ModelAssistanceService.java
+++ src/main/java/NS/pgmg/service/board/ModelAssistanceService.java
@@ -1,24 +1,21 @@
-package NS.pgmg.service;
+package NS.pgmg.service.board;
 
-import NS.pgmg.domain.board.BaseBoard;
-import NS.pgmg.domain.board.ModelAssistanceBoard;
+import NS.pgmg.domain.board.Base;
+import NS.pgmg.domain.board.ModelAssistance;
 import NS.pgmg.domain.user.User;
 import NS.pgmg.dto.board.BoardRequestDto;
 import NS.pgmg.dto.board.ModelAssistanceCreateDto;
 import NS.pgmg.dto.board.ModelAssistanceFindResponseDto;
 import NS.pgmg.dto.board.ModelAssistanceUpdateDto;
-import NS.pgmg.repository.board.ModelAssistanceBoardRepository;
+import NS.pgmg.repository.board.ModelAssistanceRepository;
 import NS.pgmg.repository.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.*;
 
 import static NS.pgmg.service.CommonMethod.*;
@@ -26,13 +23,10 @@ import static NS.pgmg.service.CommonMethod.*;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class BoardService {
+public class ModelAssistanceService {
 
-    private final ModelAssistanceBoardRepository modelAssistanceBoardRepository;
+    private final ModelAssistanceRepository modelAssistanceRepository;
     private final UserRepository userRepository;
-
-    @Value("${board.path}")
-    private String filePath;
 
     @Transactional
     public ResponseEntity<Map<String, String>> createMA(String token, ModelAssistanceCreateDto request,
@@ -40,17 +34,17 @@ public class BoardService {
         try {
             String requestEmail = tokenCheck(token);
             emailCheck(requestEmail, request.getEmail());
-            String titleValue = saveTitleFile(title);
-            List<String> detailsValue = saveDetailFiles(details);
+            String titleValue = saveFile(title);
+            List<String> detailsValue = saveFiles(details);
 
-            BaseBoard base = BaseBoard.setBaseBoard(request, requestEmail, titleValue, detailsValue);
-            ModelAssistanceBoard modelAssistanceBoard = ModelAssistanceBoard.builder()
+            Base base = Base.setBaseBoard(request, requestEmail, titleValue, detailsValue);
+            ModelAssistance modelAssistance = ModelAssistance.builder()
                     .base(base)
                     .modelAssistanceCategory(request.getModelAssistanceCategory())
                     .place(request.getPlace())
                     .price(request.getPrice())
                     .build();
-            modelAssistanceBoardRepository.save(modelAssistanceBoard);
+            modelAssistanceRepository.save(modelAssistance);
 
             return ResponseEntity.ok().body(Map.of("message", "게시물이 생성됐습니다."));
         } catch (RuntimeException e) {
@@ -62,10 +56,10 @@ public class BoardService {
     public ResponseEntity<?> findMA(BoardRequestDto request) {
         try {
             User findUser = userRepository.findByEmail(request.getEmail());
-            ModelAssistanceBoard findBoard = modelAssistanceBoardRepository.
+            ModelAssistance findBoard = modelAssistanceRepository.
                     findByBidAndBase_Email(request.getId(), request.getEmail());
 
-            BaseBoard findBase = findBoard.getBase();
+            Base findBase = findBoard.getBase();
 
             return ResponseEntity.ok().body(ModelAssistanceFindResponseDto.builder()
                     .name(findUser.getName())
@@ -94,8 +88,8 @@ public class BoardService {
     }
 
     @Transactional
-    public List<ModelAssistanceBoard> findAllMA() {
-        return modelAssistanceBoardRepository.findAll();
+    public List<ModelAssistance> findAllMA() {
+        return modelAssistanceRepository.findAll();
     }
 
     @Transactional
@@ -106,15 +100,15 @@ public class BoardService {
 
             emailCheck(requestEmail, request.getEmail());
 
-            ModelAssistanceBoard findBoard = modelAssistanceBoardRepository
+            ModelAssistance findBoard = modelAssistanceRepository
                     .findByBidAndBase_Email(request.getId(), request.getEmail());
 
-            BaseBoard base = findBoard.getBase();
+            Base base = findBoard.getBase();
 
-            String titlePath = updateTitleFile(title, base.getTitlePath());
-            List<String> detailsPaths = updateDetailFiles(details, base.getDetailPaths());
+            String titlePath = updateFile(title, base.getTitlePath());
+            List<String> detailsPaths = updateFiles(details, base.getDetailPaths());
 
-            BaseBoard updateBase = BaseBoard.builder()
+            Base updateBase = Base.builder()
                     .email(request.getEmail())
                     .title(request.getTitle())
                     .contents(request.getContents())
@@ -127,7 +121,7 @@ public class BoardService {
 
             findBoard.setUpdateBoard(updateBase, request.getModelAssistanceCategory(), request.getPlace(), request.getPrice());
 
-            modelAssistanceBoardRepository.save(findBoard);
+            modelAssistanceRepository.save(findBoard);
 
             return ResponseEntity.ok().body(Map.of("message", "게시물이 수정됐습니다."));
         } catch (RuntimeException e) {
@@ -143,108 +137,11 @@ public class BoardService {
 
             emailCheck(requestEmail, request.getEmail());
 
-            modelAssistanceBoardRepository.deleteByBid(request.getId());
+            modelAssistanceRepository.deleteByBid(request.getId());
             return ResponseEntity.ok().body(Map.of("message", "게시물이 삭제됐습니다."));
         } catch (RuntimeException e) {
             return ResponseEntity.badRequest().body(Map.of("message", e.getMessage()));
         }
 
-    }
-
-    private String saveTitleFile(MultipartFile file) {
-
-        if (file == null) {
-            return null;
-        }
-
-        String fileName = "MA-" + createRandomUuid() + ".png";
-        try {
-            file.transferTo(new File(filePath + fileName));
-        } catch (IOException e) {
-            throw new RuntimeException("Title 이미지 저장 오류", e);
-        }
-
-        return "/img/" + fileName;
-    }
-
-    private List<String> saveDetailFiles(List<MultipartFile> files) {
-
-        if (files == null) {
-            return null;
-        }
-
-        List<String> imgPaths = new ArrayList<>();
-
-        for (MultipartFile f : files) {
-            String fileName = "MA-" + createRandomUuid() + ".png";
-
-            try {
-                f.transferTo(new File(filePath + fileName));
-            } catch (IOException e) {
-                throw new RuntimeException("Details 이미지 저장 오류", e);
-            }
-            imgPaths.add("/img/" + fileName);
-        }
-
-        return imgPaths;
-    }
-
-    private String updateTitleFile(MultipartFile file, String path) {
-        String fileName;
-
-        if (file == null) {
-            return path;
-        }
-
-        if (path != null) {
-            fileName = path.replace("/img/", "");
-            File tempFile = new File(filePath + fileName);
-
-            if (tempFile.exists()) {
-                boolean delete = tempFile.delete();
-            }
-        }
-
-        fileName = "MA-" + createRandomUuid() + ".png";
-
-        try {
-            file.transferTo(new File(filePath + fileName));
-        } catch (IOException e) {
-            throw new RuntimeException("Title 이미지 저장 오류", e);
-        }
-
-        return "/img/" + fileName;
-    }
-
-    private List<String> updateDetailFiles(List<MultipartFile> files, List<String> path) {
-
-        if (files == null) {
-            return path;
-        }
-
-        List<String> paths = new ArrayList<>();
-
-        if (path != null) {
-            for (String p : path) {
-                String fileName = p.replace("/img/", "");
-                File tempFile = new File(filePath + fileName);
-
-                if (tempFile.exists()) {
-                    boolean delete = tempFile.delete();
-                }
-            }
-        }
-
-        for (MultipartFile f : files) {
-            String fileName = "MA-" + createRandomUuid() + ".png";
-            try {
-                f.transferTo(new File(filePath + fileName));
-                paths.add("/img/" + fileName);
-            } catch (IOException e) {
-                throw new RuntimeException("Details 이미지 저장 오류", e);
-            }
-        }
-
-        return paths;
     }
 }
