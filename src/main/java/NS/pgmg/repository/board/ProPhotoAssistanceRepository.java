2f5853d0813f63d44a9119ffecd59bb1680f24cd
jaemoon <woans5970@naver.com>
2024-03-28 22:07:12 +0900

    Create: ProPhotoAssistanceRepository

---
+++ src/main/java/NS/pgmg/repository/board/ProPhotoAssistanceRepository.java
@@ -0,0 +1,9 @@
+package NS.pgmg.repository.board;
+
+import NS.pgmg.domain.board.ProPhotoBoard;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProPhotoAssistanceRepository extends JpaRepository<ProPhotoBoard, Long> {
+    public ProPhotoBoard findByBidAndBase_Email(Long bid, String email);
+    public void deleteByBid(Long bid);
+}
