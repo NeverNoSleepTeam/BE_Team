7775e94c5bef0d602c6ff3f8abc85b004c0d2424
jaemoon <woans5970@naver.com>
2024-03-28 22:00:28 +0900

    Modify: FindBasicInfoResponseDto - add column

--- src/main/java/NS/pgmg/dto/userpage/FindBasicInfoResponseDto.java
+++ src/main/java/NS/pgmg/dto/userpage/FindBasicInfoResponseDto.java
@@ -3,6 +3,7 @@ package NS.pgmg.dto.userpage;
 import NS.pgmg.domain.user.enums.City;
 import NS.pgmg.domain.user.enums.Gender;
 import NS.pgmg.domain.user.enums.Nationality;
+import NS.pgmg.domain.user.enums.Rank;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,18 +19,20 @@ public class FindBasicInfoResponseDto {
     private City city;
     private Nationality nationality;
     private String intro;
+    private Rank rank;
     private List<String> profileImgPath;
     private boolean isSelf;
 
     @Builder
     public FindBasicInfoResponseDto(String email, String name, Gender gender, City city, Nationality nationality,
-                                    String intro, List<String> profileImgPath, boolean isSelf) {
+                                    String intro, Rank rank, List<String> profileImgPath, boolean isSelf) {
         this.email = email;
         this.name = name;
         this.gender = gender;
         this.city = city;
         this.nationality = nationality;
         this.intro = intro;
+        this.rank = rank;
         this.profileImgPath = profileImgPath;
         this.isSelf = isSelf;
     }
