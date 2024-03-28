f50d7a2b9a78fc75932d9eb2a507994c8ac5f625
jaemoon <woans5970@naver.com>
2024-03-28 22:01:12 +0900

    Modify: FindModelInfoResponseDto - add column

--- src/main/java/NS/pgmg/dto/userpage/FindModelInfoResponseDto.java
+++ src/main/java/NS/pgmg/dto/userpage/FindModelInfoResponseDto.java
@@ -1,14 +1,26 @@
 package NS.pgmg.dto.userpage;
 
+import NS.pgmg.domain.user.enums.City;
+import NS.pgmg.domain.user.enums.Gender;
+import NS.pgmg.domain.user.enums.Nationality;
+import NS.pgmg.domain.user.enums.Rank;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Getter
 @NoArgsConstructor
 public class FindModelInfoResponseDto {
     private String email;
     private String name;
+    private Gender gender;
+    private City city;
+    private Nationality nationality;
+    private String intro;
+    private Rank rank;
+    private List<String> profileImgPath;
     private String height;
     private String weight;
     private String top;
@@ -17,10 +29,17 @@ public class FindModelInfoResponseDto {
     private boolean isSelf;
 
     @Builder
-    public FindModelInfoResponseDto(String email, String name, String height, String weight, String top, String bottom,
-                                    String shoes, boolean isSelf) {
+    public FindModelInfoResponseDto(String email, String name, Gender gender, City city, Nationality nationality,
+                                    String intro, Rank rank, List<String> profileImgPath, String height,
+                                    String weight, String top, String bottom, String shoes, boolean isSelf) {
         this.email = email;
         this.name = name;
+        this.gender = gender;
+        this.city = city;
+        this.nationality = nationality;
+        this.intro = intro;
+        this.rank = rank;
+        this.profileImgPath = profileImgPath;
         this.height = height;
         this.weight = weight;
         this.top = top;
