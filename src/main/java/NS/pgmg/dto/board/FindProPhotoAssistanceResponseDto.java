6330a09ba6eceacca52b73938cc7458869cfe5c9
jaemoon <woans5970@naver.com>
2024-03-28 22:05:48 +0900

    Create: FindProPhotoAssistanceResponseDto

---
+++ src/main/java/NS/pgmg/dto/board/FindProPhotoAssistanceResponseDto.java
@@ -0,0 +1,62 @@
+package NS.pgmg.dto.board;
+
+import NS.pgmg.domain.board.ProPhotoCategory;
+import NS.pgmg.domain.user.enums.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+public class FindProPhotoAssistanceResponseDto {
+    private String name;
+    private String title;
+    private String contents;
+    private Integer price;
+    private String place;
+    private List<ProPhotoCategory> proPhotoCategory;
+    private LocalDateTime createdAt;
+    private String firstDate;
+    private String lastDate;
+    private Gender gender;
+    private Nationality nationality;
+    private City city;
+    private BusinessTrip businessTrip;
+    private Correction correction;
+    private Production production;
+    private String URL;
+    private String portfolioPath;
+    private String titlePath;
+    private List<String> detailPath;
+
+    @Builder
+    public FindProPhotoAssistanceResponseDto(String name, String title, String contents, Integer price, String place,
+                                             List<ProPhotoCategory> proPhotoCategory, LocalDateTime createdAt,
+                                             String firstDate, String lastDate, Gender gender, Nationality nationality,
+                                             City city, BusinessTrip businessTrip, Correction correction,
+                                             Production production, String URL, String portfolioPath, String titlePath,
+                                             List<String> detailPath) {
+        this.name = name;
+        this.title = title;
+        this.contents = contents;
+        this.price = price;
+        this.place = place;
+        this.proPhotoCategory = proPhotoCategory;
+        this.createdAt = createdAt;
+        this.firstDate = firstDate;
+        this.lastDate = lastDate;
+        this.gender = gender;
+        this.nationality = nationality;
+        this.city = city;
+        this.businessTrip = businessTrip;
+        this.correction = correction;
+        this.production = production;
+        this.URL = URL;
+        this.portfolioPath = portfolioPath;
+        this.titlePath = titlePath;
+        this.detailPath = detailPath;
+    }
+}
