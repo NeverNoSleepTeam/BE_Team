c0a0a0ced59d493a613dd64d88372b95d1494512
jaemoon <woans5970@naver.com>
2024-03-28 22:06:09 +0900

    Create: ModelNeedRepository

---
+++ src/main/java/NS/pgmg/repository/board/ModelNeedRepository.java
@@ -0,0 +1,9 @@
+package NS.pgmg.repository.board;
+
+import NS.pgmg.domain.board.ModelBoard;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ModelNeedRepository extends JpaRepository<ModelBoard, Long> {
+    public ModelBoard findByBidAndBase_Email(Long bid, String email);
+    public void deleteByBid(Long bid);
+}
