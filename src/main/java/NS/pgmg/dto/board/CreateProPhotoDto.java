35814ee20921a7ce952ed255c7a5b7f211532c24
jaemoon <woans5970@naver.com>
2024-03-28 22:05:25 +0900

    Create: CreateProPhotoDto

---
+++ src/main/java/NS/pgmg/dto/board/CreateProPhotoDto.java
@@ -0,0 +1,34 @@
+package NS.pgmg.dto.board;
+
+import NS.pgmg.domain.board.ProPhotoCategory;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+public class CreateProPhotoDto {
+    private String email;
+    private String title;
+    private String contents;
+    private Integer price;
+    private String place;
+    private List<ProPhotoCategory> proPhotoCategory;
+    private String firstDate;
+    private String lastDate;
+
+    @Builder
+    public CreateProPhotoDto(String email, String title, String contents, Integer price, String place,
+                             List<ProPhotoCategory> proPhotoCategory, String firstDate, String lastDate) {
+        this.email = email;
+        this.title = title;
+        this.contents = contents;
+        this.price = price;
+        this.place = place;
+        this.proPhotoCategory = proPhotoCategory;
+        this.firstDate = firstDate;
+        this.lastDate = lastDate;
+    }
+}
