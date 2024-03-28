3520666dfc6d4d1ce7079a01594a5bf02c29e46e
jaemoon <woans5970@naver.com>
2024-03-28 22:05:53 +0900

    Create: FindProPhotoNeedResponseDto

---
+++ src/main/java/NS/pgmg/dto/board/FindProPhotoNeedResponseDto.java
@@ -0,0 +1,42 @@
+package NS.pgmg.dto.board;
+
+import NS.pgmg.domain.board.ProPhotoCategory;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+public class FindProPhotoNeedResponseDto {
+    private String name;
+    private String title;
+    private String contents;
+    private Integer price;
+    private String place;
+    private List<ProPhotoCategory> proPhotoCategory;
+    private LocalDateTime createdAt;
+    private String firstDate;
+    private String lastDate;
+    private String titlePath;
+    private List<String> detailPath;
+
+    @Builder
+    public FindProPhotoNeedResponseDto(String name, String title, String contents, Integer price, String place,
+                                       List<ProPhotoCategory> proPhotoCategory, LocalDateTime createdAt, String firstDate,
+                                       String lastDate, String titlePath, List<String> detailPath) {
+        this.name = name;
+        this.title = title;
+        this.contents = contents;
+        this.price = price;
+        this.place = place;
+        this.proPhotoCategory = proPhotoCategory;
+        this.createdAt = createdAt;
+        this.firstDate = firstDate;
+        this.lastDate = lastDate;
+        this.titlePath = titlePath;
+        this.detailPath = detailPath;
+    }
+}
