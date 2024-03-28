87a2765b23e85b620cd88541bd149709e660213d
jaemoon <woans5970@naver.com>
2024-03-28 22:02:06 +0900

    Modify: ModelAssistanceController - modify API URL, rename function

--- src/main/java/NS/pgmg/controller/board/ModelAssistanceController.java
+++ src/main/java/NS/pgmg/controller/board/ModelAssistanceController.java
@@ -1,12 +1,11 @@
 package NS.pgmg.controller.board;
 
-import NS.pgmg.domain.board.ModelAssistance;
+import NS.pgmg.domain.board.ModelBoard;
 import NS.pgmg.dto.board.BoardRequestDto;
-import NS.pgmg.dto.board.ModelAssistanceCreateDto;
-import NS.pgmg.dto.board.ModelAssistanceUpdateDto;
+import NS.pgmg.dto.board.CreateModelDto;
+import NS.pgmg.dto.board.UpdateModelDto;
 import NS.pgmg.service.board.ModelAssistanceService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,51 +16,48 @@ import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/board")
-@ResponseStatus(HttpStatus.CREATED)
+@RequestMapping("/board/model/assistance")
 public class ModelAssistanceController {
 
     private final ModelAssistanceService modelAssistanceService;
 
-    @PostMapping(value = "/model-assistance",
-            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
-    public ResponseEntity<Map<String, String>> createModelAssistanceBoard(
+    @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
+    public ResponseEntity<Map<String, String>> create(
             @RequestHeader(value = "Token") String token,
-            @RequestPart(value = "RequestBody") ModelAssistanceCreateDto modelAssistanceCreateDto,
+            @RequestPart(value = "RequestBody") CreateModelDto createModelDto,
             @RequestPart(value = "Title", required = false) MultipartFile title,
             @RequestPart(value = "Details", required = false) List<MultipartFile> details
     ) {
-        return modelAssistanceService.createMA(token, modelAssistanceCreateDto, title, details);
+        return modelAssistanceService.create(token, createModelDto, title, details);
     }
 
-    @PostMapping("/model-assistance/detail")
-    public ResponseEntity<?> findModelAssistanceBoard(
+    @PostMapping("/detail")
+    public ResponseEntity<?> find(
             @RequestBody BoardRequestDto boardRequestDto
     ) {
-        return modelAssistanceService.findMA(boardRequestDto);
+        return modelAssistanceService.find(boardRequestDto);
     }
 
-    @GetMapping("/model-assistance")
-    public List<ModelAssistance> findAllModelAssistanceBoard() {
-        return modelAssistanceService.findAllMA();
+    @GetMapping
+    public List<ModelBoard> findAll() {
+        return modelAssistanceService.findAll();
     }
 
-    @PutMapping(value = "/model-assistance",
-            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
-    public ResponseEntity<Map<String, String>> updateModelAssistanceBoard(
+    @PutMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
+    public ResponseEntity<Map<String, String>> update(
             @RequestHeader(value = "Token") String token,
-            @RequestPart(value = "RequestBody", required = false) ModelAssistanceUpdateDto modelAssistanceUpdateDto,
+            @RequestPart(value = "RequestBody", required = false) UpdateModelDto updateModelDto,
             @RequestPart(value = "Title", required = false) MultipartFile title,
             @RequestPart(value = "Details", required = false) List<MultipartFile> details
     ) {
-        return modelAssistanceService.updateMA(token, modelAssistanceUpdateDto, title, details);
+        return modelAssistanceService.update(token, updateModelDto, title, details);
     }
 
-    @DeleteMapping("/model-assistance")
-    public ResponseEntity<Map<String, String>> deleteModelAssistanceBoard(
+    @DeleteMapping
+    public ResponseEntity<Map<String, String>> delete(
             @RequestHeader(value = "Token") String token,
             @RequestBody BoardRequestDto boardRequestDto
     ) {
-        return modelAssistanceService.deleteMA(token, boardRequestDto);
+        return modelAssistanceService.delete(token, boardRequestDto);
     }
 }
