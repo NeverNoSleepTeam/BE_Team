2094a588ae7015e675374543270f43d02daebe5e
jaemoon <woans5970@naver.com>
2024-03-28 22:08:42 +0900

    Create: UpdateTalentDonationDto

---
+++ src/main/java/NS/pgmg/dto/board/UpdateTalentDonationDto.java
@@ -0,0 +1,29 @@
+package NS.pgmg.dto.board;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Getter
+@NoArgsConstructor
+public class UpdateTalentDonationDto {
+    private Long id;
+    private String email;
+    private String title;
+    private String contents;
+    private String place;
+    private String firstDate;
+    private String lastDate;
+
+    @Builder
+    public UpdateTalentDonationDto(String email, String title, String contents, String place, String firstDate,
+                                   String lastDate) {
+        this.email = email;
+        this.title = title;
+        this.contents = contents;
+        this.place = place;
+        this.firstDate = firstDate;
+        this.lastDate = lastDate;
+    }
+}
