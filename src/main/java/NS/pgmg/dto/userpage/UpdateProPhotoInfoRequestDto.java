f57ef13774b94e65083ac05d21b6f873df862283
jaemoon <woans5970@naver.com>
2024-03-29 16:17:33 +0900

    Create: UpdateProPhotoInfoRequestDto

---
+++ src/main/java/NS/pgmg/dto/userpage/UpdateProPhotoInfoRequestDto.java
@@ -0,0 +1,20 @@
+package NS.pgmg.dto.userpage;
+
+import NS.pgmg.domain.user.enums.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class UpdateProPhotoInfoRequestDto {
+    private String email;
+    private String passwd;
+    private Gender gender;
+    private Nationality nationality;
+    private City city;
+    private String intro;
+    private BusinessTrip businessTrip;
+    private Correction correction;
+    private Production production;
+    private String URL;
+}
