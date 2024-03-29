22436abf3c7ea9c5a97ed6ba5e28a337a607ebc1
jaemoon <woans5970@naver.com>
2024-03-29 16:17:25 +0900

    Create: UpdateModelInfoRequestDto

---
+++ src/main/java/NS/pgmg/dto/userpage/UpdateModelInfoRequestDto.java
@@ -0,0 +1,23 @@
+package NS.pgmg.dto.userpage;
+
+import NS.pgmg.domain.user.enums.City;
+import NS.pgmg.domain.user.enums.Gender;
+import NS.pgmg.domain.user.enums.Nationality;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class UpdateModelInfoRequestDto {
+    private String email;
+    private String passwd;
+    private Gender gender;
+    private Nationality nationality;
+    private City city;
+    private String intro;
+    private String height;
+    private String weight;
+    private String top;
+    private String bottom;
+    private String shoes;
+}
