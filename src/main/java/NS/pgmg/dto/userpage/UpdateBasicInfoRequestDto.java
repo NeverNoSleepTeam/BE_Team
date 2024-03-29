29cb2f20e38f3f4883356cb117aee06c41bcf4c1
jaemoon <woans5970@naver.com>
2024-03-29 16:13:39 +0900

    Modify: UpdateBasicInfoRequestDto - add NoArgsConstructor

--- src/main/java/NS/pgmg/dto/userpage/UpdateBasicInfoRequestDto.java
+++ src/main/java/NS/pgmg/dto/userpage/UpdateBasicInfoRequestDto.java
@@ -4,8 +4,10 @@ import NS.pgmg.domain.user.enums.City;
 import NS.pgmg.domain.user.enums.Gender;
 import NS.pgmg.domain.user.enums.Nationality;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class UpdateBasicInfoRequestDto {
     private String email;
     private String passwd;
