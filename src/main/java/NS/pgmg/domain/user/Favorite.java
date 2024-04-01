65ab96da458cf28064ababd76e8a53b329b3cb0e
jaemoon <woans5970@naver.com>
2024-04-01 16:38:36 +0900

    Create: Favorite

---
+++ src/main/java/NS/pgmg/domain/user/Favorite.java
@@ -0,0 +1,54 @@
+package NS.pgmg.domain.user;
+
+import NS.pgmg.domain.user.enums.City;
+import NS.pgmg.domain.user.enums.Nationality;
+import NS.pgmg.domain.user.enums.UserRank;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class Favorite {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long fid;
+
+    private String email;
+
+    private String name;
+
+    private UserRank fUserRank;
+
+    private String fHeight;
+
+    private String fWeight;
+
+    private String fTop;
+
+    private String fBottom;
+
+    private String fShoes;
+
+    private City fCity;
+
+    private Nationality fNationality;
+
+    @Builder
+    public Favorite(String email, String name, UserRank fUserRank, String fHeight, String fWeight, String fTop,
+                    String fBottom, String fShoes, City fCity, Nationality fNationality) {
+        this.email = email;
+        this.name = name;
+        this.fUserRank = fUserRank;
+        this.fHeight = fHeight;
+        this.fWeight = fWeight;
+        this.fTop = fTop;
+        this.fBottom = fBottom;
+        this.fShoes = fShoes;
+        this.fCity = fCity;
+        this.fNationality = fNationality;
+    }
+}
