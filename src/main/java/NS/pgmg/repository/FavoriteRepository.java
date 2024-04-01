07af0207278c0572530f9aef81aa15380d9838cf
jaemoon <woans5970@naver.com>
2024-04-01 16:38:54 +0900

    Create: FavoriteRepository

---
+++ src/main/java/NS/pgmg/repository/FavoriteRepository.java
@@ -0,0 +1,12 @@
+package NS.pgmg.repository;
+
+import NS.pgmg.domain.user.Favorite;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
+    public void deleteByEmailAndName(String email, String fName);
+
+    public List<Favorite> findAllByEmail(String email);
+}
