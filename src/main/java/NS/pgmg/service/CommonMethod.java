c9a90a14d0ffe2f73ce0e092a20d17c40b2d0ce6
jaemoon <woans5970@naver.com>
2024-04-04 23:39:02 +0900

    Modify: CommonMethod - add @Component

--- src/main/java/NS/pgmg/service/CommonMethod.java
+++ src/main/java/NS/pgmg/service/CommonMethod.java
@@ -3,6 +3,7 @@ package NS.pgmg.service;
 import NS.pgmg.config.JwtConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
@@ -12,6 +13,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Slf4j
+@Component
 public final class CommonMethod {
 
     public static String imgPath;
@@ -58,7 +60,7 @@ public final class CommonMethod {
             deleteFile(fileName, pdfPath);
         }
 
-        if (file != null) {
+        if (file != null && !file.isEmpty()) {
             fileName = createRandomUuid() + ".pdf";
 
             try {
@@ -82,11 +84,13 @@ public final class CommonMethod {
             deleteFile(fileName, imgPath);
         }
 
-        if (file != null) {
+        log.info("file = {}", file);
+        if (file != null && !file.isEmpty()) {
             fileName = createRandomUuid() + ".png";
 
             try {
                 file.transferTo(new File(imgPath + fileName));
+                log.info("savedPath = {}", imgPath + fileName);
             } catch (IOException e) {
                 throw new RuntimeException("이미지 저장 오류", e);
             }
