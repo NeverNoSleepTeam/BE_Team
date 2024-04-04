db81342e3e4cac5a2bf12562bf04cf64ae3718c7
jaemoon <woans5970@naver.com>
2024-04-04 21:55:32 +0900

    Modify: structure

--- src/main/java/NS/pgmg/repository/board/TalentDonationRepository.java
+++ src/main/java/NS/pgmg/repository/board/TalentDonationRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TalentDonationRepository extends JpaRepository<TalentDonationBoard, Long> {
     public TalentDonationBoard findByBidAndBaseBoard_Email(Long bid, String email);
+
     public void deleteByBid(Long bid);
 }
