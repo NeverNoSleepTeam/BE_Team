8741a38b642cb56e8b2cd1c530e7b9cea3fe756f
jaemoon <woans5970@naver.com>
2024-04-04 21:52:03 +0900

    Modify: remove @Builder

--- src/main/java/NS/pgmg/dto/userpage/FindByNameDto.java
+++ src/main/java/NS/pgmg/dto/userpage/FindByNameDto.java
@@ -1,17 +1,10 @@
 package NS.pgmg.dto.userpage;
 
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class FindByNameDto {
-
     private String name;
-
-    @Builder
-    public FindByNameDto(String name) {
-        this.name = name;
-    }
 }
