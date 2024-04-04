d26049ac424f289082ecfb0b4ae8e71a2a7d0355
jaemoon <woans5970@naver.com>
2024-04-04 21:53:54 +0900

    Modify: add findAllBigCategory function

--- src/main/java/NS/pgmg/repository/board/ModelAssistanceRepository.java
+++ src/main/java/NS/pgmg/repository/board/ModelAssistanceRepository.java
@@ -1,9 +1,15 @@
 package NS.pgmg.repository.board;
 
+import NS.pgmg.domain.board.BigCategory;
 import NS.pgmg.domain.board.ModelBoard;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ModelAssistanceRepository extends JpaRepository<ModelBoard, Long> {
     public ModelBoard findByBidAndBaseBoard_Email(Long bid, String email);
+
+    public List<ModelBoard> findAllByBigCategory(BigCategory bigCategory);
+
     public void deleteByBid(Long bid);
 }
--- src/main/java/NS/pgmg/repository/board/PhotoShopRepository.java
+++ src/main/java/NS/pgmg/repository/board/PhotoShopRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PhotoShopRepository extends JpaRepository<PhotoShopBoard, Long> {
     public PhotoShopBoard findByBidAndBaseBoard_Email(Long bid, String email);
+
     public void deleteByBid(Long bid);
 }
