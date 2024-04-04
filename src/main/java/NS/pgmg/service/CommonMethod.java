c94df05dcb1c4a1fd3680fc545fa498035c60866
jaemoon <woans5970@naver.com>
2024-04-04 21:49:50 +0900

    Modify: CommonMethod - setting @Value

--- src/main/java/NS/pgmg/service/CommonMethod.java
+++ src/main/java/NS/pgmg/service/CommonMethod.java
@@ -1,6 +1,7 @@
 package NS.pgmg.service;
 
 import NS.pgmg.config.JwtConfig;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -10,10 +11,22 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-public class CommonMethod {
+@Slf4j
+public final class CommonMethod {
 
-    @Value("${board.path}")
-    public static String boardPath;
+    public static String imgPath;
+    public static String pdfPath;
+
+    @Value("${img.path}")
+    public void setImgPath(String imgPath) {
+        CommonMethod.imgPath = imgPath;
+    }
+
+    @Value("${pdf.path}")
+    public void setPdfPath(String pdfPath) {
+        CommonMethod.pdfPath = pdfPath;
+    }
+
 
     public static String tokenCheck(String token) {
 
@@ -35,8 +48,90 @@ public class CommonMethod {
     public static String createRandomUuid() {
         return UUID.randomUUID().toString();
     }
-    
-//    public static String saveFile(MultipartFile file, String filePath) {
+
+    public static String updatePDF(MultipartFile file, String path) {
+
+        String fileName;
+
+        if (path != null) {
+            fileName = path.replace("/portfolio/", "");
+            deleteFile(fileName, pdfPath);
+        }
+
+        if (file != null) {
+            fileName = createRandomUuid() + ".pdf";
+
+            try {
+                file.transferTo(new File(pdfPath + fileName));
+            } catch (IOException e) {
+                throw new RuntimeException("pdf 저장 오류", e);
+            }
+
+            return "/portfolio/" + fileName;
+        }
+
+        return null;
+    }
+
+    public static String updateImgFile(MultipartFile file, String path) {
+
+        String fileName;
+
+        if (path != null) {
+            fileName = path.replace("/img/", "");
+            deleteFile(fileName, imgPath);
+        }
+
+        if (file != null) {
+            fileName = createRandomUuid() + ".png";
+
+            try {
+                file.transferTo(new File(imgPath + fileName));
+            } catch (IOException e) {
+                throw new RuntimeException("이미지 저장 오류", e);
+            }
+
+            return "/img/" + fileName;
+        }
+        return null;
+    }
+
+    public static List<String> updateImgFiles(List<MultipartFile> files, List<String> path) {
+
+        List<String> paths = new ArrayList<>();
+
+        if (path != null) {
+            for (String p : path) {
+                String fileName = p.replace("/img/", "");
+                deleteFile(fileName, imgPath);
+            }
+        }
+
+        if (files != null) {
+            for (MultipartFile f : files) {
+                String fileName = createRandomUuid() + ".png";
+                try {
+                    f.transferTo(new File(imgPath + fileName));
+                    paths.add("/img/" + fileName);
+                } catch (IOException e) {
+                    throw new RuntimeException("이미지 저장 오류", e);
+                }
+            }
+            return paths;
+        }
+
+        return null;
+    }
+
+    public static void deleteFile(String fileName, String filePath) {
+        File tempFile = new File(filePath + fileName);
+
+        if (tempFile.exists()) {
+            boolean delete = tempFile.delete();
+        }
+    }
+
+    //    public static String saveFile(MultipartFile file, String filePath) {
 //
 //        if (file == null) {
 //            return null;
@@ -73,62 +168,4 @@ public class CommonMethod {
 //
 //        return imgPaths;
 //    }
-
-    public static String updateFile(MultipartFile file, String path, String filePath) {
-
-        String fileName;
-
-        if (path != null) {
-            fileName = path.replace("/img/", "");
-            deleteFile(fileName, filePath);
-        }
-
-        if (file != null) {
-            fileName = createRandomUuid() + ".png";
-
-            try {
-                file.transferTo(new File(filePath + fileName));
-            } catch (IOException e) {
-                throw new RuntimeException("이미지 저장 오류", e);
-            }
-
-            return "/img/" + fileName;
-        }
-        return null;
-    }
-
-    public static List<String> updateFiles(List<MultipartFile> files, List<String> path, String filePath) {
-
-        List<String> paths = new ArrayList<>();
-
-        if (path != null) {
-            for (String p : path) {
-                String fileName = p.replace("/img/", "");
-                deleteFile(fileName, filePath);
-            }
-        }
-
-        if (files != null) {
-            for (MultipartFile f : files) {
-                String fileName = createRandomUuid() + ".png";
-                try {
-                    f.transferTo(new File(filePath + fileName));
-                    paths.add("/img/" + fileName);
-                } catch (IOException e) {
-                    throw new RuntimeException("이미지 저장 오류", e);
-                }
-            }
-            return paths;
-        }
-
-        return null;
-    }
-
-    protected static void deleteFile(String fileName, String filePath) {
-        File tempFile = new File(filePath + fileName);
-
-        if (tempFile.exists()) {
-            boolean delete = tempFile.delete();
-        }
-    }
 }
