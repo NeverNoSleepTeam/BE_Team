e427526f5a7502c6490f0fe8326d1552f414e2d6
jaemoon <woans5970@naver.com>
2024-04-04 21:55:19 +0900

    Modify: add findAllBigCategory function

--- src/main/java/NS/pgmg/repository/board/ProPhotoNeedRepository.java
+++ src/main/java/NS/pgmg/repository/board/ProPhotoNeedRepository.java
@@ -1,9 +1,15 @@
 package NS.pgmg.repository.board;
 
+import NS.pgmg.domain.board.BigCategory;
 import NS.pgmg.domain.board.ProPhotoBoard;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProPhotoNeedRepository extends JpaRepository<ProPhotoBoard, Long> {
     public ProPhotoBoard findByBidAndBaseBoard_Email(Long bid, String email);
+
+    public List<ProPhotoBoard> findAllByBigCategory(BigCategory bigCategory);
+
     public void deleteByBid(Long bid);
 }
