cc6867b90066cb83717c83e859c9f0bc1611ce27
jaemoon <woans5970@naver.com>
2024-04-19 19:52:30 +0900

    Modify: UserRepository - add findAllByIsModelOrderByUidDesc

--- src/main/java/NS/pgmg/repository/user/UserRepository.java
+++ src/main/java/NS/pgmg/repository/user/UserRepository.java
@@ -3,8 +3,12 @@ package NS.pgmg.repository.user;
 import NS.pgmg.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     public User findByEmail(String email);
 
     public User findByName(String name);
+
+    public List<User> findAllByIsModelOrderByUidDesc(boolean model);
 }
