4dabdc444147a729bbb77003d594165bab496495
jaemoon <woans5970@naver.com>
2024-04-28 22:13:29 +0900

    Modify: ChatRoomRepository - modify function

--- src/main/java/NS/pgmg/service/chat/ChatService.java
+++ src/main/java/NS/pgmg/service/chat/ChatService.java
@@ -2,80 +2,91 @@ package NS.pgmg.service.chat;
 
 import NS.pgmg.domain.chat.Chat;
 import NS.pgmg.domain.chat.ChatRoom;
-import NS.pgmg.domain.chat.ChatType;
-import NS.pgmg.dto.chat.EnterAndExitDto;
-import NS.pgmg.dto.chat.FindRoomDto;
+import NS.pgmg.domain.user.User;
+import NS.pgmg.dto.chat.ChatViewDto;
+import NS.pgmg.dto.chat.FindChatDto;
 import NS.pgmg.dto.chat.SendMessageDto;
 import NS.pgmg.repository.chat.ChatRepository;
 import NS.pgmg.repository.chat.ChatRoomRepository;
+import NS.pgmg.repository.user.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Map;
 
 import static NS.pgmg.service.CommonMethod.emailCheck;
 import static NS.pgmg.service.CommonMethod.tokenCheck;
 
+
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ChatService {
 
-    private final RabbitTemplate rabbitTemplate;
+    private final UserRepository userRepository;
     private final ChatRepository chatRepository;
     private final ChatRoomRepository chatRoomRepository;
 
+    @Transactional
     public ResponseEntity<?> sendMessage(String roomId, SendMessageDto request) {
 
-//        try {
-//            String tokenEmail = tokenCheck(token);
-//            emailCheck(tokenEmail, request.getEmail());
-//        } catch (RuntimeException e) {
-//            return ResponseEntity.badRequest().body(Map.of("message", e));
-//        }
+        User findUser = userRepository.findByName(request.getReceiverName());
 
-        Chat chat = Chat.builder()
-                .type(ChatType.TALK)
-                .roomId(roomId)
-                .sender(request.getSender())
-                .receiver(request.getReceiver())
-                .content(request.getContent())
-                .build();
-//
-//        ChatRoom findSenderRoom = chatRoomRepository.findByRoomIdAndSender(roomId, request.getSender());
-//        ChatRoom findReceiverRoom = chatRoomRepository.findByRoomIdAndSender(roomId, request.getReceiver());
-//
-//        if (findReceiverRoom == null) {
-//            ChatRoom newRoom = ChatRoom.builder()
-//                    .roomId(roomId)
-//                    .lastMessage(request.getContent())
-//                    .sender(request.getReceiver())
-//                    .receiver(request.getSender())
-//                    .build();
-//
-//            chatRoomRepository.save(newRoom);
-//        } else {
-//            findReceiverRoom.updateLastMessage(request.getContent());
-//            chatRoomRepository.save(findReceiverRoom);
-//        }
-//
-//        findSenderRoom.updateLastMessage(request.getContent());
-//        chatRoomRepository.save(findSenderRoom);
+        String senderEmail = request.getSenderEmail();
+        String receiverEmail = findUser.getEmail();
 
-        rabbitTemplate.convertAndSend("chat.exchange", "room." + roomId, chat);
-        return ResponseEntity.ok().body(chatRepository.save(chat));
-    }
+        ChatRoom findSenderRoom = chatRoomRepository.findBySenderEmailAndReceiverEmail(senderEmail, receiverEmail);
+        ChatRoom findReceiverRoom = chatRoomRepository.findBySenderEmailAndReceiverEmail(receiverEmail, senderEmail);
 
-    public ResponseEntity<?> findAllMessage(String token, FindRoomDto request) {
-
-        try {
-            String tokenEmail = tokenCheck(token);
-            emailCheck(tokenEmail, request.getEmail());
-        } catch (RuntimeException e) {
-            return ResponseEntity.badRequest().body(Map.of("message", e));
+        if (findReceiverRoom == null) {
+            findReceiverRoom = ChatRoom.builder()
+                    .roomId(roomId)
+                    .senderEmail(receiverEmail)
+                    .receiverEmail(senderEmail)
+                    .build();
         }
 
-        return ResponseEntity.ok().body(chatRepository.findAllByRoomIdAndSender(request.getRoomId(), request.getSender()));
+        findSenderRoom.updateLastMessage(request.getContent());
+        findReceiverRoom.updateLastMessage(request.getContent());
+
+        chatRoomRepository.save(findSenderRoom);
+        chatRoomRepository.save(findReceiverRoom);
+
+        Chat senderChat = Chat.builder()
+                .roomId(roomId)
+                .senderEmail(request.getSenderEmail())
+                .receiverEmail(findUser.getEmail())
+                .content(request.getContent())
+                .build();
+
+        chatRepository.save(senderChat);
+
+        ChatViewDto view = ChatViewDto.builder()
+                .roomId(roomId)
+                .senderEmail(request.getSenderEmail())
+                .content(senderChat.getContent())
+                .createdAt(senderChat.getCreatedAt())
+                .build();
+
+        return ResponseEntity.ok().body(view);
+    }
+
+    @Transactional
+    public ResponseEntity<?> getChats(String token, FindChatDto request) {
+
+        try {
+            String requestEmail = tokenCheck(token);
+            emailCheck(requestEmail, request.getSenderEmail());
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(Map.of("message", e.getMessage()));
+        }
+
+        List<Chat> findChatList = chatRepository.findAllByRoomId(request.getRoomId());
+
+        return ResponseEntity.ok().body(findChatList);
     }
 }
