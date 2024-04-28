b82fda6840f6da1987d07d34f13ecf00ed85df76
jaemoon <woans5970@naver.com>
2024-04-28 22:12:28 +0900

    Modify: ChatRepository - modify function name

--- src/main/java/NS/pgmg/repository/chat/ChatRepository.java
+++ src/main/java/NS/pgmg/repository/chat/ChatRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.List;
 
 public interface ChatRepository extends MongoRepository<Chat, String> {
-    public List<Chat> findAllByRoomIdAndSender(String roomId, String sender);
+    public List<Chat> findAllByRoomId(String roomId);
 }
