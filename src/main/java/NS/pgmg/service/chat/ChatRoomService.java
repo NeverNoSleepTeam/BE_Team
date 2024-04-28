b473777416365270304684b694ad427ed3c0749a
jaemoon <woans5970@naver.com>
2024-04-28 22:14:16 +0900

    Modify: ChatRoomRepository - modify function

--- src/main/java/NS/pgmg/service/chat/ChatRoomService.java
+++ src/main/java/NS/pgmg/service/chat/ChatRoomService.java
@@ -1,74 +1,140 @@
 package NS.pgmg.service.chat;
 
 import NS.pgmg.domain.chat.ChatRoom;
-import NS.pgmg.dto.chat.EnterAndExitDto;
+import NS.pgmg.domain.chat.RoomHistory;
+import NS.pgmg.domain.user.User;
+import NS.pgmg.dto.chat.CreateRoomDto;
+import NS.pgmg.dto.chat.DeleteRoomDto;
 import NS.pgmg.dto.chat.FindAllRoomDto;
+import NS.pgmg.dto.chat.FindAllRoomResponseDto;
 import NS.pgmg.repository.chat.ChatRoomRepository;
+import NS.pgmg.repository.chat.RoomHistoryRepository;
+import NS.pgmg.repository.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Map;
+import java.util.*;
 
-import static NS.pgmg.service.CommonMethod.emailCheck;
-import static NS.pgmg.service.CommonMethod.tokenCheck;
+import static NS.pgmg.service.CommonMethod.*;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ChatRoomService {
 
+    private final UserRepository userRepository;
     private final ChatRoomRepository chatRoomRepository;
+    private final RoomHistoryRepository roomHistoryRepository;
 
-    public ResponseEntity<?> createRoom(String token, String roomId, EnterAndExitDto request) {
+    @Transactional
+    public ResponseEntity<?> createRooms(String token, CreateRoomDto request) {
 
         try {
-            String tokenEmail = tokenCheck(token);
-            emailCheck(tokenEmail, request.getEmail());
+            String requestEmail = tokenCheck(token);
+            emailCheck(requestEmail, request.getSenderEmail());
         } catch (RuntimeException e) {
-            return ResponseEntity.badRequest().body(Map.of("message", e));
+            return ResponseEntity.badRequest().body(Map.of("message", e.getMessage()));
         }
 
-        ChatRoom senderRoom = ChatRoom.builder()
+        User findUser = userRepository.findByName(request.getReceiverName());
+
+        String senderEmail = request.getSenderEmail();
+        String receiverEmail = findUser.getEmail();
+
+        ChatRoom findRoom = chatRoomRepository.findBySenderEmailAndReceiverEmail(senderEmail, receiverEmail);
+
+        RoomHistory findHistory = roomHistoryRepository.findBySenderEmailAndReceiverEmail(senderEmail, receiverEmail);
+
+        String roomId = createRandomUuid().substring(16) + createRandomUuid().substring(0, 15);
+
+        if (findRoom != null) {
+            return ResponseEntity.badRequest().body(Map.of("message", "이미 존재하는 채팅방입니다."));
+        } else if (findHistory != null) {
+            ChatRoom chatRoom = ChatRoom.builder()
+                    .roomId(findHistory.getRoomId())
+                    .senderEmail(senderEmail)
+                    .receiverEmail(receiverEmail)
+                    .build();
+            chatRoomRepository.save(chatRoom);
+
+            return ResponseEntity.ok().body(Map.of("message", "채팅방이 생성됐습니다."));
+        }
+
+        ChatRoom savedSenderRoom = ChatRoom.builder()
                 .roomId(roomId)
-                .sender(request.getSender())
-                .receiver(request.getReceiver())
+                .senderEmail(senderEmail)
+                .receiverEmail(receiverEmail)
                 .build();
 
-        ChatRoom receiverRoom = ChatRoom.builder()
+        ChatRoom savedReceiverRoom = ChatRoom.builder()
                 .roomId(roomId)
-                .sender(request.getReceiver())
-                .receiver(request.getSender())
+                .senderEmail(receiverEmail)
+                .receiverEmail(senderEmail)
                 .build();
 
-        chatRoomRepository.save(senderRoom);
-        chatRoomRepository.save(receiverRoom);
+        RoomHistory savedSenderHistory = RoomHistory.builder()
+                .roomId(roomId)
+                .senderEmail(senderEmail)
+                .receiverEmail(receiverEmail)
+                .build();
 
-        return ResponseEntity.ok(Map.of("message", "채팅방이 생성됐습니다."));
+        RoomHistory savedReceiverHistory = RoomHistory.builder()
+                .roomId(roomId)
+                .senderEmail(receiverEmail)
+                .receiverEmail(senderEmail)
+                .build();
+
+        chatRoomRepository.save(savedSenderRoom);
+        chatRoomRepository.save(savedReceiverRoom);
+        roomHistoryRepository.save(savedSenderHistory);
+        roomHistoryRepository.save(savedReceiverHistory);
+
+        return ResponseEntity.ok().body(Map.of("message", "채팅방이 생성됐습니다."));
     }
 
-    public ResponseEntity<?> findAllRooms(String token, FindAllRoomDto request) {
+    @Transactional
+    public ResponseEntity<?> getChatRooms(String token, FindAllRoomDto request) {
 
+        log.info("call getChatRooms");
         try {
-            String tokenEmail = tokenCheck(token);
-            emailCheck(tokenEmail, request.getEmail());
+            String requestEmail = tokenCheck(token);
+            emailCheck(requestEmail, request.getSenderEmail());
         } catch (RuntimeException e) {
-            return ResponseEntity.badRequest().body(Map.of("message", e));
+            return ResponseEntity.badRequest().body(Map.of("message", e.getMessage()));
         }
 
-        return ResponseEntity.ok().body(chatRoomRepository.findAllBySender(request.getSender()));
+        List<ChatRoom> findRooms = chatRoomRepository.findAllBySenderEmail(request.getSenderEmail());
+
+        List<FindAllRoomResponseDto> responseRooms = new ArrayList<>();
+        for (ChatRoom findRoom : findRooms) {
+            User receiverUser = userRepository.findByEmail(findRoom.getReceiverEmail());
+
+            FindAllRoomResponseDto responseRoom = FindAllRoomResponseDto.builder()
+                    .roomId(findRoom.getRoomId())
+                    .senderEmail(findRoom.getSenderEmail())
+                    .receiverName(receiverUser.getName())
+                    .lastMessage(findRoom.getLastMessage())
+                    .build();
+
+            responseRooms.add(responseRoom);
+        }
+        return ResponseEntity.ok().body(responseRooms);
     }
 
-    public ResponseEntity<?> deleteRoom(String token, String roomId, EnterAndExitDto request) {
+    @Transactional
+    public ResponseEntity<?> deleteChatRoom(String token, DeleteRoomDto request) {
 
         try {
-            String tokenEmail = tokenCheck(token);
-            emailCheck(tokenEmail, request.getEmail());
+            String requestEmail = tokenCheck(token);
+            emailCheck(requestEmail, request.getSenderEmail());
         } catch (RuntimeException e) {
-            return ResponseEntity.badRequest().body(Map.of("message", e));
+            return ResponseEntity.badRequest().body(Map.of("message", e.getMessage()));
         }
 
-        chatRoomRepository.deleteByRoomIdAndSender(roomId, request.getSender());
-
+        chatRoomRepository.deleteByRoomIdAndSenderEmail(request.getRoomId(), request.getSenderEmail());
         return ResponseEntity.ok().body(Map.of("message", "채팅방이 삭제됐습니다."));
     }
 }
