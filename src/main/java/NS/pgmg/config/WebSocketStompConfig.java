87e6c8217943b6965fc9620d24126070d2b906f9
jaemoon <woans5970@naver.com>
2024-04-28 22:16:12 +0900

    Modify: WebSocketStompConfig - add handler

--- src/main/java/NS/pgmg/config/WebSocketStompConfig.java
+++ src/main/java/NS/pgmg/config/WebSocketStompConfig.java
@@ -1,25 +1,36 @@
 package NS.pgmg.config;
 
+import NS.pgmg.Handler.StompErrorHandler;
+import NS.pgmg.Handler.StompHandler;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.util.AntPathMatcher;
 import org.springframework.web.socket.config.annotation.*;
 
 @Configuration
+@RequiredArgsConstructor
 @EnableWebSocketMessageBroker
 public class WebSocketStompConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final StompHandler stompHandler;
+    private final StompErrorHandler stompErrorHandler;
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
                 .setAllowedOriginPatterns("*");
+        registry.setErrorHandler(stompErrorHandler);
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-//        registry.setPathMatcher(new AntPathMatcher(".")); // url / -> .
         registry.setApplicationDestinationPrefixes("/pub");
         registry.enableSimpleBroker("/sub");
-//        registry.enableStompBrokerRelay("/queue", "/topic", "/exchange", "/amq/queue");
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(stompHandler);
     }
 }
