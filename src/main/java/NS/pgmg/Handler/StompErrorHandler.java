1b9a19e61873f09f59879349de62f6599fcce2ee
jaemoon <woans5970@naver.com>
2024-04-28 22:17:19 +0900

    Create: StompErrorHandler

---
+++ src/main/java/NS/pgmg/Handler/StompErrorHandler.java
@@ -0,0 +1,34 @@
+package NS.pgmg.Handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.StompSubProtocolErrorHandler;
+
+import java.nio.charset.StandardCharsets;
+
+@Slf4j
+@Component
+public class StompErrorHandler extends StompSubProtocolErrorHandler {
+
+    @Override
+    public Message<byte[]> handleClientMessageProcessingError(Message<byte[]> clientMessage, Throwable ex) {
+        if ("UNAUTHORIZED".equals(ex.getMessage())) {
+            return errorMessage();
+        }
+
+        return super.handleClientMessageProcessingError(clientMessage, ex);
+    }
+
+    private Message<byte[]> errorMessage() {
+
+        StompHeaderAccessor accessor = StompHeaderAccessor.create(StompCommand.ERROR);
+        accessor.setLeaveMutable(true);
+
+        return MessageBuilder.createMessage("권한이 없습니다.".getBytes(StandardCharsets.UTF_8),
+                accessor.getMessageHeaders());
+    }
+}
