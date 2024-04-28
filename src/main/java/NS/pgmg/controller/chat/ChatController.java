59d76c7b664b4198a5a2b032f821c16255d6bfed
jaemoon <woans5970@naver.com>
2024-04-28 22:12:09 +0900

    Modify: ChatController add swagger info

--- src/main/java/NS/pgmg/controller/chat/ChatController.java
+++ src/main/java/NS/pgmg/controller/chat/ChatController.java
@@ -1,41 +1,41 @@
 package NS.pgmg.controller.chat;
 
-import NS.pgmg.dto.chat.FindRoomDto;
+import NS.pgmg.dto.chat.FindChatDto;
 import NS.pgmg.dto.chat.SendMessageDto;
 import NS.pgmg.service.chat.ChatService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.handler.annotation.DestinationVariable;
-import org.springframework.messaging.handler.annotation.Header;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.*;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
+@Tag(name = "Chat", description = "채팅 관련 API")
 @RestController
 @RequiredArgsConstructor
 public class ChatController {
 
     private final ChatService chatService;
 
-    // 채팅방 대화
-    @MessageMapping("talk.{roomId}")
-    public ResponseEntity<?> talk(
-//            @Header(value = "Token", required = false) String token,
+    @MessageMapping("/{roomId}")
+    @SendTo("/sub/{roomId}")
+    public ResponseEntity<?> sendMessage(
             @DestinationVariable(value = "roomId") String roomId,
-            @Payload SendMessageDto request) {
-//        return chatService.sendMessage(token, roomId, request);
+            @Payload SendMessageDto request
+    ) {
         return chatService.sendMessage(roomId, request);
     }
 
+
+    @Operation(summary = "전체 채팅 조회")
     @PostMapping("/chat")
-    public ResponseEntity<?> findAllMessage(
+    public ResponseEntity<?> getChats(
             @RequestHeader(value = "Token") String token,
-            @RequestBody FindRoomDto request) {
-        return chatService.findAllMessage(token, request);
+            @RequestBody FindChatDto request
+    ) {
+        return chatService.getChats(token, request);
     }
 }
