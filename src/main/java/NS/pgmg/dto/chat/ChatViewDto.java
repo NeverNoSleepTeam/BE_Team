aefcdad86cf59084ba068dd4d228853953f0cb65
jaemoon <woans5970@naver.com>
2024-05-02 23:03:11 +0900

    Modify: ChatViewDto - add column

--- src/main/java/NS/pgmg/dto/chat/ChatViewDto.java
+++ src/main/java/NS/pgmg/dto/chat/ChatViewDto.java
@@ -1,5 +1,6 @@
 package NS.pgmg.dto.chat;
 
+import NS.pgmg.domain.chat.ChatType;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,14 +10,15 @@ import java.time.LocalDateTime;
 @Getter
 @NoArgsConstructor
 public class ChatViewDto {
-
+    private ChatType type;
     private String roomId;
     private String senderEmail;
     private String content;
     private LocalDateTime createdAt;
 
     @Builder
-    public ChatViewDto(String roomId, String senderEmail, String content, LocalDateTime createdAt) {
+    public ChatViewDto(ChatType type, String roomId, String senderEmail, String content, LocalDateTime createdAt) {
+        this.type = type;
         this.roomId = roomId;
         this.senderEmail = senderEmail;
         this.content = content;
