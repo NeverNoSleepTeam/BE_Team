644e2dd68e58700c7aafeede996eacbb83dbb10a
jaemoon <woans5970@naver.com>
2024-05-02 23:04:18 +0900

    Modify: FindAllRoomResponseDto - add column

--- src/main/java/NS/pgmg/dto/chat/FindAllRoomResponseDto.java
+++ src/main/java/NS/pgmg/dto/chat/FindAllRoomResponseDto.java
@@ -1,5 +1,6 @@
 package NS.pgmg.dto.chat;
 
+import NS.pgmg.domain.user.enums.UserRank;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,14 +10,21 @@ import lombok.NoArgsConstructor;
 public class FindAllRoomResponseDto {
     private String roomId;
     private String senderEmail;
+    private String receiverEmail;
     private String receiverName;
+    private UserRank receiverRank;
+    private String receiverImgURL;
     private String lastMessage;
 
     @Builder
-    public FindAllRoomResponseDto(String roomId, String senderEmail, String receiverName, String lastMessage) {
+    public FindAllRoomResponseDto(String roomId, String receiverEmail, String senderEmail, String receiverName,
+                                  UserRank receiverRank, String receiverImgURL, String lastMessage) {
         this.roomId = roomId;
         this.senderEmail = senderEmail;
+        this.receiverEmail = receiverEmail;
         this.receiverName = receiverName;
+        this.receiverRank = receiverRank;
+        this.receiverImgURL = receiverImgURL;
         this.lastMessage = lastMessage;
     }
 }
