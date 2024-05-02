d686e78338b884f14d27fd1491393b7b4fd44b60
jaemoon <woans5970@naver.com>
2024-05-02 23:05:13 +0900

    Modify: MessageController - change name

--- src/main/java/NS/pgmg/controller/chat/MessageController.java
+++ src/main/java/NS/pgmg/controller/chat/MessageController.java
@@ -1,8 +1,10 @@
 package NS.pgmg.controller.chat;
 
 import NS.pgmg.dto.chat.FindChatDto;
+import NS.pgmg.dto.chat.SavePromiseDto;
 import NS.pgmg.dto.chat.SendMessageDto;
-import NS.pgmg.service.chat.ChatService;
+import NS.pgmg.dto.chat.SendPromiseDto;
+import NS.pgmg.service.chat.MessageService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -16,19 +18,18 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "Chat", description = "채팅 관련 API")
 @RestController
 @RequiredArgsConstructor
-public class ChatController {
+public class MessageController {
 
-    private final ChatService chatService;
+    private final MessageService messageService;
 
-    @MessageMapping("/{roomId}")
+    @MessageMapping("/{roomId}/talk")
     @SendTo("/sub/{roomId}")
     public ResponseEntity<?> sendMessage(
             @DestinationVariable(value = "roomId") String roomId,
             @Payload SendMessageDto request
     ) {
-        return chatService.sendMessage(roomId, request);
+        return messageService.sendMessage(roomId, request);
     }
-
 
     @Operation(summary = "전체 채팅 조회")
     @PostMapping("/chat")
@@ -36,6 +37,23 @@ public class ChatController {
             @RequestHeader(value = "Token") String token,
             @RequestBody FindChatDto request
     ) {
-        return chatService.getChats(token, request);
+        return messageService.getMessages(token, request);
+    }
+
+    @MessageMapping("/{roomId}/promise")
+    @SendTo("/sub/{roomId}")
+    public ResponseEntity<?> sendPromise(
+            @DestinationVariable(value = "roomId") String roomId,
+            @Payload SendPromiseDto request
+    ) {
+        return messageService.sendPromise(roomId, request);
+    }
+
+    @Operation(summary = "약속 저장")
+    @PostMapping("/promise")
+    public ResponseEntity<?> savePromise(
+            @RequestBody SavePromiseDto request
+    ) {
+        return messageService.savePromise(request);
     }
 }
