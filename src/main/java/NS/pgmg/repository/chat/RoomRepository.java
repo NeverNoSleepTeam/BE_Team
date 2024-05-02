efa163c17a3be92b7a9d223cf5fe0ce18b01a0a7
jaemoon <woans5970@naver.com>
2024-05-02 23:10:45 +0900

    Modify: RoomRepository - change name

--- src/main/java/NS/pgmg/repository/chat/RoomRepository.java
+++ src/main/java/NS/pgmg/repository/chat/RoomRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
 
-public interface ChatRoomRepository extends MongoRepository<ChatRoom, String> {
+public interface RoomRepository extends MongoRepository<ChatRoom, String> {
     public ChatRoom findBySenderEmailAndReceiverEmail(String senderEmail, String receiverEmail);
     public List<ChatRoom> findAllBySenderEmail(String senderEmail);
     public void deleteByRoomIdAndSenderEmail(String roomId, String senderId);
